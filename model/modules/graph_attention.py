bce5bf234f205d88f9ee52e3fa1500d7d3bdc5df
HOjaeSon <mebam@naver.com>
2025-05-11 00:53:16 +0000

    1

--- model/modules/graph_attention.py
+++ model/modules/graph_attention.py
@@ -52,18 +52,17 @@ class SpatialAPPNP(nn.Module):
     def __init__(self, dim:int, j_or_e:CachedGraph, appnp_iter:int, checkpoint_length:int=0, drop:float=0.0):
         super().__init__()
         self.human_graph = j_or_e
-        self.appnp1 = GATAppnp(dim, appnp_iter, checkpoint_length=checkpoint_length)
-        self.norm = nn.LayerNorm(dim)
+        self.appnp1 = GATBlock(dim, appnp_iter, checkpoint_length=checkpoint_length)
         self.drop = nn.Dropout(drop)
 
     def forward(self, x: torch.Tensor):
         # B * T are spatial batch dimension.
         # Consider x.shape: [B, T, J, C].
-        x = self.drop(self.appnp1(self.norm(x), self.human_graph))
+        x = self.drop(self.appnp1(x, self.human_graph))
         return x
 
 class SpatialBlock(nn.Module):
-    def __init__(self, dim, appnp_iter:int=8, use_grad_checkpont:bool=True, mlp_ratio:float=4.0, drop:float=0.0):
+    def __init__(self, dim, appnp_iter:int=4, use_grad_checkpont:bool=True, mlp_ratio:float=4.0, drop:float=0.0):
         super().__init__()
         len_ckpt = appnp_iter // 4 if use_grad_checkpont else 0
         self.sj = SpatialAPPNP(dim, j_graph, appnp_iter=appnp_iter, checkpoint_length=len_ckpt , drop=drop)
@@ -90,18 +89,17 @@ class SpatialBlock(nn.Module):
         joint = self.sj(x)
         a = F.softmax(self.fusion(torch.cat([joint, bone], dim=-1)), dim=-1)
 
-        x = x + a[..., 0:1] * joint + a[..., 1:2] * bone
+        x = a[..., 0:1] * joint + a[..., 1:2] * bone
         x = x + self.mlp_out(self.norm(x))
         return x
 
-class GATAppnp(nn.Module):
-    def __init__(self, dim:int, n_iter:int, checkpoint_length:int|None=None):
+class GATBlock(nn.Module):
+    def __init__(self, dim:int, n_iter:int, checkpoint_length:int=0):
         super().__init__()
-        self.alpha = nn.Linear(dim, 1)
-        nn.init.xavier_normal_(self.alpha.weight.data)
-        nn.init.xavier_normal_(self.alpha.bias.data)
-        self.norm = nn.LayerNorm(dim)
-        self.conv = GAT(dim)
+        self.alpha = nn.ModuleList([nn.Linear(dim, 1) for i in range(n_iter)])
+        self.norm = nn.ModuleList([nn.LayerNorm(dim) for i in range(n_iter)])
+        self.linear = nn.ModuleList([MLP(dim, 4*dim, dim) for i in range(n_iter)])
+        self.conv = nn.ModuleList([GAT(dim) for i in range(n_iter)])
         self.n_iter = n_iter
         self.checkpoint_length = checkpoint_length or 0
         self.n_ckpt_iter = n_iter // checkpoint_length if self.checkpoint_length > 0 else 0
@@ -117,15 +115,16 @@ class GATAppnp(nn.Module):
             x = self.appnp_loop(x, x0, g, self.n_iter)
         return x
     
-    def single_step(self, x:torch.Tensor, x0:torch.Tensor, g:CachedGraph):
-            x = self.norm(x)
-            skip_rate = F.sigmoid(self.alpha(x))
-            x = (1.0 - skip_rate) * self.conv(x, g) + skip_rate * x0
+    def single_step(self, x:torch.Tensor, x0:torch.Tensor, g:CachedGraph, i:int):
+            x = self.linear[i](x)
+            x = self.norm[i](x)
+            skip_rate = F.sigmoid(self.alpha[i](x))
+            x = (1.0 - skip_rate) * self.conv[i](x, g) + skip_rate * x0
             return x
     
     def appnp_loop(self, x:torch.Tensor, x0:torch.Tensor, g:CachedGraph, iter:int):
-        for _ in range(iter):
-            x = self.single_step(x, x0, g)
+        for i in range(iter):
+            x = self.single_step(x, x0, g, i)
         return x
 
 class GAT(nn.Module):
@@ -200,8 +199,6 @@ def test_spatial_graph_attention():
     show_mem(f"before ---")
     output = model(x)
     show_mem(f"after ---")
-
-    print(output[0,0,0])
     assert not torch.allclose(output, x), "Model not modifying input"
 
 if __name__ == '__main__':
